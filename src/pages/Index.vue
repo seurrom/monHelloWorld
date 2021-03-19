99eed7d7dacd3506e44e525fb2d647cf8dcb66a2
Seuret Romane <romane.seuret@divtec.ch>
2021-03-19 13:23:59 +0100

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex flex-center">
+    Hello World
+    <q-btn
+        class="q-mt-xl"
+        color="white"
+        text-color="blue"
+        unelevated
+        to="/Romane"
+        label="Go to Romane"
+        no-caps
+      />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
---
+++ src/pages/Romane.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Romane
+    <q-btn
+        class="q-mt-xl"
+        color="white"
+        text-color="blue"
+        unelevated
+        to="/"
+        label="Go Home"
+        no-caps
+      />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Romane'
+}
+</script>
